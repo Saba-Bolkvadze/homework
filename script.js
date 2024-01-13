a6644fd00798691b7c2afb2ab8245e152f9bb7c2
saba-bolkvadze <sababolqvadze54@gmail.com>
2024-01-13 13:02:27 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let celsiusi1 = 23;
+let farenheiti1 = (celsiusi1 * 9/5) + 32;
+
+document.getElementById("celsiusi1").innerHTML = celsiusi1;
+document.getElementById("farenheiti1").innerHTML = farenheiti1;
+
+let celsiusi2 = 56;
+let farenheiti2 = (celsiusi2 * 9/5) + 32;
+
+document.getElementById("celsiusi2").innerHTML = celsiusi2;
+document.getElementById("farenheiti2").innerHTML = farenheiti2;
+
+let celsiusi3 = 21;
+let farenheiti3 = (celsiusi3 * 9/5) + 32;
+
+document.getElementById("celsiusi3").innerHTML = celsiusi3;
+document.getElementById("farenheiti3").innerHTML = farenheiti3;
+
+let celsiusi4 = 55;
+let farenheiti4 = (celsiusi4 * 9/5) + 32;
+ 
+document.getElementById("celsiusi4").innerHTML = celsiusi4;
+document.getElementById("farenheiti4").innerHTML = farenheiti4
